6f7ffcfb17796c86ad8d3865861f204012d53c2f
pierry01 <pierrybm@gmail.com>
2019-06-27 02:49:14 +0000

    Matchers de Errors

--- spec/calculator/calculator_spec.rb
+++ spec/calculator/calculator_spec.rb
@@ -1,6 +1,15 @@
 require 'calculator'
 
 describe Calculator do
+  context '#div with' do # Usar {#} para testar erro
+    it 'divided by 0' do
+      expect{subject.div(3, 0)}.to raise_error(ZeroDivisionError)
+      expect{subject.div(3, 0)}.to raise_error('divided by 0')
+      expect{subject.div(3, 0)}.to raise_error(ZeroDivisionError, 'divided by 0')
+      expect{subject.div(3, 0)}.to raise_error(/divided/)
+    end
+  end
+  
   context '#sum with' do
     it 'positive numbers' do
       result = subject.sum(5, 5)
