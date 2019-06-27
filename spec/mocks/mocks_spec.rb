3aeee9c3b9b31f62fcb916c292f7696806ec5419
pierry01 <pierrybm@gmail.com>
2019-06-27 07:53:47 +0000

    Mock Expectations

--- spec/mocks/mocks_spec.rb
+++ spec/mocks/mocks_spec.rb
@@ -3,13 +3,29 @@ require 'course'
 
 describe 'Mocks' do
   it '#bar' do
-    # setup 1
+    student = Student.new # setup (1)
+    expect(student).to receive(:bar) # verify (3)
+    student.bar # exercise (2)
+  end
+  
+  it 'args' do
     student = Student.new
-    
-    # verify 3
-    expect(student).to receive(:bar)
-    
-    # exercise 2
-    student.bar
+    expect(student).to receive(:foo).with(123)
+    student.foo(123)
+  end
+  
+  it 'repetição' do
+    # .once, .twice, .exactly(x).times, .at_least(x).times, .at_least(:once)
+    student = Student.new
+    expect(student).to receive(:foo).with(123).twice
+    student.foo(123)
+    student.foo(123)
+  end
+  
+  it 'retorno' do
+    # .once, .twice, .exactly(x).times, .at_least(x).times, .at_least(:once)
+    student = Student.new
+    expect(student).to receive(:foo).with(123).and_return(true)
+    student.foo(123)
   end
 end
