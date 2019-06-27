db6076fd36478817bff449974aa8f3c16b0a669d
pierry01 <pierrybm@gmail.com>
2019-06-27 04:05:40 +0000

    Hooks (before e after)

--- spec/matchers/atributos/atributos_spec.rb
+++ spec/matchers/atributos/atributos_spec.rb
@@ -1,11 +1,25 @@
 require 'pessoa'
 
 describe 'Atributos' do
+  before(:each) do
+    @pessoa = Pessoa.new
+  end
+  
+  # after(:each) do
+  #   @pessoa = Pessoa.new
+  # end
+  
   it 'have_attributes' do
-    pessoa = Pessoa.new
-    pessoa.nome = 'Jean'
-    pessoa.idade = 21
+    @pessoa.nome = 'Jean'
+    @pessoa.idade = 21
     
-    expect(pessoa).to have_attributes(nome: starting_with('J'), idade: (be >= 21))
+    expect(@pessoa).to have_attributes(nome: starting_with('J'), idade: (be >= 21))
+  end
+  
+  it 'have_attributes' do
+    @pessoa.nome = 'Junfo'
+    @pessoa.idade = 25
+    
+    expect(@pessoa).to have_attributes(nome: starting_with('J'), idade: (be >= 21))
   end
 end
