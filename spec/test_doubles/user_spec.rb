d6465bdac7da454217b30711ebfef30a3449b35d
pierry01 <pierrybm@gmail.com>
2019-06-27 07:56:07 +0000

    As Null Object

--- spec/test_doubles/user_spec.rb
+++ spec/test_doubles/user_spec.rb
@@ -13,4 +13,13 @@ describe 'Test Double' do
     user.name
     user.password
   end
+  
+  it 'as_null_object' do
+    user = double('User').as_null_object
+    allow(user).to receive(:name).and_return('Jean')
+    allow(user).to receive(:password).and_return('secret')
+    user.name
+    user.password
+    user.abc
+  end
 end
