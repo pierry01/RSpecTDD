d38a64328b445dc3bf8ab90c00917306e43b8930
pierry01 <pierrybm@gmail.com>
2019-06-27 07:28:50 +0000

    correção

--- spec/test_doubles/user_spec.rb
+++ spec/test_doubles/user_spec.rb
@@ -2,15 +2,15 @@ describe 'Test Double' do
   it '#' do
     user = double('User')
     allow(user).to receive_messages(name: 'Jean', password: 'secret')
-    puts user.name
-    puts user.password
+    user.name
+    user.password
   end
   
   it '##' do
     user = double('User')
     allow(user).to receive(:name).and_return('Jean')
     allow(user).to receive(:password).and_return('secret')
-    puts user.name
-    puts user.password
+    user.name
+    user.password
   end
 end
