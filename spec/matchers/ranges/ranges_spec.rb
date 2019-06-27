22eb3b50760e590de12c191c5fd55ff9c83015e1
pierry01 <pierrybm@gmail.com>
2019-06-27 03:09:12 +0000

    One-liner Syntax (subject)

--- spec/matchers/ranges/ranges_spec.rb
+++ spec/matchers/ranges/ranges_spec.rb
@@ -2,4 +2,8 @@ describe (1..5), 'Ranges' do
   it '#cover' do
     expect(subject).to cover(1, 2, 3, 4, 5)
   end
+  
+  # expect(subject) = is_expected
+  it { is_expected.to cover(2) }
+  it { is_expected.to cover(1, 2, 3, 4, 5) }
 end
