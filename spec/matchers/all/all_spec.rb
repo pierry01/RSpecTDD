7294bc1cac1c60ba02f69aa1a2c4f0fa6af1030c
pierry01 <pierrybm@gmail.com>
2019-06-27 06:58:42 +0000

    Tag Filters

--- spec/matchers/all/all_spec.rb
+++ spec/matchers/all/all_spec.rb
@@ -1,4 +1,5 @@
-describe 'all' do
-  it { expect([1, 3, 5]).to all ((be_odd).and be_an(Integer)) }
+describe 'all', type: 'collection' do
+  # rspec . -t type:collection
+  it { expect([1,3,5]).to all ((be_odd).and be_an(Integer)) }
   it { expect(['ruby', 'rails']).to all(be_a(String).and include('r')) }
 end
--- spec/matchers/arrays/arrays_spec.rb
+++ spec/matchers/arrays/arrays_spec.rb
@@ -1,6 +1,7 @@
 RSpec::Matchers.define_negated_matcher :exclude, :include
 
-describe Array.new([1,2,3]), 'Array' do
+describe Array.new([1,2,3]), 'Array', type: 'collection' do
+  # rspec . -t type:collection
   it '#include' do # Elementos separados do Array
     is_expected.to include(3,1,2)
   end
@@ -8,7 +9,7 @@ describe Array.new([1,2,3]), 'Array' do
   it { is_expected.to exclude(4) }
   
   it '#contain_exactly' do # Contém exatamente os elementos
-    is_expected.to contain_exactly(3, 2, 1)
+    is_expected.to contain_exactly(3,2,1)
   end
   
   it '#match_array' do # Exatamente igual ao Array
