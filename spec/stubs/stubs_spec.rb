bf85f58bf8572e006c8b74a6822a547717d875dd
pierry01 <pierrybm@gmail.com>
2019-06-27 07:38:26 +0000

    Method Stubs

--- spec/stubs/stubs_spec.rb
+++ spec/stubs/stubs_spec.rb
@@ -15,4 +15,37 @@ describe 'Stub' do
     
     expect(course_finished).to be_truthy
   end
+  
+  it 'Argumentos Dinâmicos' do
+    student = Student.new
+    
+    allow(student).to receive(:foo) do |arg|
+      if arg == :hello
+        'olá'
+      elsif arg == :hi
+        'hi!!'
+      end
+    end
+    
+    expect(student.foo(:hello)).to eq('olá')
+    expect(student.foo(:hi)).to eq('hi!!')
+  end
+  
+  it 'Qualquer instância de classe' do
+    student = Student.new
+    other_student = Student.new
+    
+    allow_any_instance_of(Student).to receive(:bar).and_return(true)
+    
+    expect(student.bar).to be_truthy
+    expect(other_student.bar).to be_truthy
+  end
+  
+  it 'Erros' do
+    student = Student.new
+
+    allow(student).to receive(:bar).and_raise(RuntimeError)
+    
+    expect{ student.bar }.to raise_error(RuntimeError)
+  end
 end
